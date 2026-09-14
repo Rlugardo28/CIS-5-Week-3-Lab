802f1e4994a1d4b910c0a757c980c7c768a119ef
Raymundo Lugardo <raymundo.lugardo55@gmail.com>
2026-09-14 21:02:12 +0000

    Competed assigment

--- main.cpp
+++ main.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
 #include <string>
 
-// Lab 3 — Your Name
+// Lab 3 — Raymundo Lugardo
 // CIS 5 Week 03 · Types & variables
-
-int main() {
+using std::cout;
+using std::cin;
+using std::string;
+using std::endl;
+int main() 
+{
   const int CURRENT_YEAR = 2026;
 
   // TODO: declare and initialize on the same line
@@ -13,9 +17,25 @@ int main() {
   // double height_m = ...;
   // char initial = ...;
   // bool student = ...;
-
   std::cout << "=== About me ===\n";
-  // TODO: labeled prints from the variable names + CURRENT_YEAR
+   std::string name = "Raymundo Lugardo";
+  cout << "Name:" << name << endl;
+  int Age = 26;
+  cout << "Age:" << Age << endl;
+  double height_m = 1.78;
+  cout << "Heght(m):" << height_m << endl;
+  char initial = 'R';
+  cout << "Initial:" << initial << endl;
+  bool student = false;
+  cout << "Student:" << student << endl;
+  cout << "Year:" << CURRENT_YEAR << endl;
 
-  return 0;
+  // TODO: labeled prints from the variable names + CURRENT_YEAR
+  
+  
+  
+  
+  
+
+   return 0;
 }
